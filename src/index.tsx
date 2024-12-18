bcd9e7c4fdd568e09748ca3b20c1b68c33ba9fb0
Yuri Golikov <iurii.golikov@gmail.com>
2024-12-18 16:45:16 +0100

    Initialized Tip Calculator App component with simple HTML and CSS

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import {BrowserRouter, Route, Routes} from "react-router";
-import SplitTheBill from "./routes/SplitTheBill.tsx"
-import Index from "./routes/Index.tsx";
+import {BrowserRouter, Route, Routes} from 'react-router'
+import Index from './routes/Index.tsx'
+import App from './components/App.tsx'
+import SplitTheBill from './routes/SplitTheBill.tsx'
 import './index.css'
+import './main.css'
 
 createRoot(window.document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route index element={<Index/>}/>
+        <Route path="/tip-calculator" element={<App/>}/>
         <Route path="/split-the-bill" element={<SplitTheBill/>}/>
       </Routes>
     </BrowserRouter>
