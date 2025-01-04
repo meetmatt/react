9ba4e71c46cad98b717ec17ff622d125d2fa6e03
Yuri Golikov <iurii.golikov@gmail.com>
2025-01-04 11:13:44 +0100

    Added router, separated the apps

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import App from './components/App.tsx'
+import {BrowserRouter, Route, Routes} from "react-router";
+import SplitTheBill from "./routes/SplitTheBill.tsx"
+import Index from "./routes/Index.tsx";
 import './index.css'
 
 createRoot(window.document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Index/>}/>
+        <Route path="/split-the-bill" element={<SplitTheBill/>}/>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ src/routes/Index.tsx
@@ -0,0 +1,9 @@
+import {Link} from "react-router";
+
+export default function Index() {
+  return (
+    <ul>
+      <li><Link to='/split-the-bill'>Split the Bill</Link></li>
+    </ul>
+  )
+}
